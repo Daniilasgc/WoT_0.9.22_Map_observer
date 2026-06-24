f33d0fedf841266c390fbc62fe91500002579e72
Daniilasgc <119393879+Daniilasgc@users.noreply.github.com>
2026-06-24 21:14:39 +0300

    Delete 0.9.22.0/res directory

--- 0.9.22.0/res/scripts/client/gui/mods/offhangar/logging.py
+++
@@ -1,6 +0,0 @@
-import functools
-from gui.mods.offhangar.utils import *
-
-doLog = functools.partial(doLog, 'OFFHANGAR')
-LOG_NOTE = functools.partial(doLog, '[NOTE]')
-LOG_DEBUG = functools.partial(doLog, '[DEBUG]')
